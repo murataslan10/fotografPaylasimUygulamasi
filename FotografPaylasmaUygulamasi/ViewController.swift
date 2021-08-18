07ab3c98b1219de64847cf45626c3f5ecb606fc1
murataslan10 <aslanmrt11@gmail.com>
2021-08-18 14:02:44 +0300

    Initial Commit

---
+++ FotografPaylasmaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotografPaylasmaUygulamasi
+//
+//  Created by Merve dalbeler on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
